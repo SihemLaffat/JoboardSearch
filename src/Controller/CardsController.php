ed95cd940f0ed883a084dc3af78e4c29d4f1e548
Sahby <sahby.mhenni@gmail.com>
2021-02-08 17:16:17 +0100

    css

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -6,12 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class CardsController extends AbstractController
 {
     /**
      * @Route("/cards", name="cards")
      */
     public function index(): Response
+    {
+        return $this->render('cards/index.html.twig', [
+            'controller_name' => 'CardsController',
+        ]);
+    }
+
+    /**
+     * @Route("/all_cards", name="cards_all")
+     */
+    public function cardsAll(): Response
     {
         return $this->render('cards/index.html.twig', [
             'controller_name' => 'CardsController',
