25ecb13cbb342fd30aa4eee540d204bf2259daa1
natcoline <natcoline@gmail.com>
2021-02-08 09:36:04 +0100

    header footer base et accueil

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+// Page Jquery : Avant de commencer, je m'assure dans la console que le dom est entièrement chargé
+
+$("document").ready(function (){
+    console.log("jQuery Ok le dom est entièrement chargé !");
+
+
+
+});
---
+++ public/js/scripts.js
@@ -0,0 +1,1 @@
+/* Page JavaScript */
