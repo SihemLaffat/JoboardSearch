956c7a2c1a6fc88c58c77f98f40551941d33a784
natcoline <natcoline@gmail.com>
2021-02-09 09:32:17 +0100

    valider

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -32,15 +32,7 @@ class SecurityController extends AbstractController
     public function logout()
     {
         //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-
-        /* return $this->render('accueil/accueil.html.twig', [
-            'controller_name' => 'AccueilController',
-        ]); */
-        
-        return $this->redirectToRoute(('target_accueil'));
-    }
-
-    
+    }  
 }
 
 
