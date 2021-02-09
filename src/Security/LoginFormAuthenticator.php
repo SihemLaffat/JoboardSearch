0634014ace58a29a59cfc0b22b169510adc8b7f5
natcoline <natcoline@gmail.com>
2021-02-09 17:48:07 +0100

    Mise à jour des liens du main

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -96,7 +96,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('utilisateur'));
+        return new RedirectResponse($this->urlGenerator->generate('cards'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
