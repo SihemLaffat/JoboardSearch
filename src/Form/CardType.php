6116886f4115687b2768362a731db6bed50b5425
Sahby <sahby.mhenni@gmail.com>
2021-02-10 10:49:39 +0100

    Crud card

---
+++ src/Form/CardType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Card;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CardType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('ville')
+            ->add('telephone')
+            ->add('email')
+            ->add('createdAt')
+            ->add('status_card')
+            ->add('utilisateur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Card::class,
+        ]);
+    }
+}
