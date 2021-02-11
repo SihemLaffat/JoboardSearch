9d1b53dd97bf92d33bff4f6a44169fb76cc6def1
sihem laffat <76011183+SihemLaffat@users.noreply.github.com>
2021-02-11 10:25:51 +0100

    sihem cardscontroller

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CardRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class CardsController extends AbstractController
@@ -12,10 +13,18 @@ class CardsController extends AbstractController
     /**
      * @Route("/cards", name="cards")
      */
-    public function index(): Response
-    {
+    public function index(CardRepository $cardRepository): Response
+    {   
+        $user =$this->getUser(); 
+        $cards = $cardRepository->findBy(['utilisateur' => $user]);
+        
+        $vaPostuler =[];
+        $postule =[];
+        
+
         return $this->render('cards/index.html.twig', [
             'controller_name' => 'CardsController',
+            'cardAll' => $cards
         ]);
     }
 
