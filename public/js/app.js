494cff486b474291d5673dca9289db825614bb6c
sihem laffat <76011183+SihemLaffat@users.noreply.github.com>
2021-02-14 01:27:12 +0100

    style acceuil

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,25 @@
+//JQuery
+$(document).ready(function() {
+    
+    //Smooth scroll down
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate( {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+
+
+    //Initialisation AOS
+    AOS.init({
+        easing: 'ease',
+        duration: 2000,
+        once: true
+    });
+});
+
+
+
+
 var ul = document.getElementsByTagName('ul')
 var choice = document.getElementsByClassName('choice');
 var dragItem = null;
@@ -39,3 +61,37 @@ function dragLeave() {
     e.preventDefault();
     
 }
+
+
+/* Slider */
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+display(index);
+function display (index) {
+	slides.forEach((slide) => {
+		slide.style.display = 'none';
+	});
+	slides[index].style.display = 'flex';
+};
+
+function nextSlide () {
+	index++;
+	if (index > slides.length - 1) {
+		index = 0;
+	}
+	display(index);
+};
+
+function prevSlide () {
+	index--;
+	if (index < 0) {
+		index = slides.length - 1;
+	}
+	display(index);
+};
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
